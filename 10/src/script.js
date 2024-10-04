1f676969afe1359a05a8d964018bc6e82b616a70
David Vrankar <david.vrankar97@gmail.com>
2024-10-04 18:18:05 +0200

    init 10 final

--- 10/src/script.js
+++ 10/src/script.js
@@ -6,29 +6,29 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
  */
 const loadingManager = new THREE.LoadingManager()
 
-loadingManager.onStart = () =>
-{
-    console.log('onStart')
-}
+// loadingManager.onStart = () =>
+// {
+//     console.log('onStart')
+// }
 
 
-loadingManager.onLoad = () =>
-{
-    console.log('onLoad')
-}
+// loadingManager.onLoad = () =>
+// {
+//     console.log('onLoad')
+// }
 
-loadingManager.onProgress = () =>
-{
-    console.log('onProgress')
-}
+// loadingManager.onProgress = () =>
+// {
+//     console.log('onProgress')
+// }
 
-loadingManager.onError = () =>
-{
-    console.log('onError')
-}
+// loadingManager.onError = () =>
+// {
+//     console.log('onError')
+// }
 
 const textureLoader = new THREE.TextureLoader(loadingManager)
-const colorTexture = textureLoader.load('/textures/door/color.jpg')
+const colorTexture = textureLoader.load('/textures/minecraft.png')
 colorTexture.colorSpace = THREE.SRGBColorSpace  
 const alphaTexture = textureLoader.load('/textures/door/alpha.jpg')
 const heightTexture = textureLoader.load('/textures/door/height.jpg')
@@ -38,6 +38,21 @@ const ambientOcclusionTexture = textureLoader.load('/textures/door/ambientOcclus
 const metalnessTexture = textureLoader.load('/textures/door/metalness.jpg')
 const roughnessTexture = textureLoader.load('/textures/door/roughness.jpg')
 
+// colorTexture.repeat.x = 2
+// colorTexture.repeat.y = 3
+// colorTexture.wrapS = THREE.MirroredRepeatWrapping
+// colorTexture.wrapT = THREE.MirroredRepeatWrapping
+
+// colorTexture.offset.x = 0.5
+// colorTexture.offset.y = 0.5
+
+// colorTexture.rotation = Math.PI * 0.25
+// colorTexture.center.x = 0.5
+// colorTexture.center.y = 0.5
+
+colorTexture.generateMipmaps = false
+colorTexture.minFilter = THREE.NearestFilter
+colorTexture.magFilter = THREE.NearestFilter
 
 /**
  * Base

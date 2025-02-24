abdd70829bb9118431f02bb8d0b412ec78f1c281
David Vrankar <david.vrankar97@gmail.com>
2025-02-24 16:00:47 +0100

    init 54 final

---
+++ 54/src/App.js
@@ -0,0 +1,48 @@
+import { useMemo, useState } from 'react'
+import Clicker from './Clicker.js'
+import People from './people.jsx'
+
+export default function App({ clickersCount, children })
+{
+    const [ hasClicker, setHasClicker ] = useState(true)
+    const [ count, setCount ] = useState(0)
+
+    const toggleClickerClick = () =>
+    {
+        setHasClicker(!hasClicker)
+    }
+
+    const increment = () =>
+    {
+        setCount (count + 1)
+    }
+
+    const colors = useMemo(() => {
+        const colors =[]
+
+        for(let i = 0; i < clickersCount; i++)
+            colors.push(`hsl(${ Math.random() * 360 }deg, 100%, 70%)`)
+
+        return colors
+    }, [ clickersCount ])
+
+    return <>
+        { children }
+
+        <div>Total count: { count }</div>
+
+        <button onClick={ toggleClickerClick }>{ hasClicker ? 'hide' : 'show' } Clicker</button>
+
+        { hasClicker && <>
+            { [...Array(clickersCount)].map((value, index) =>
+                <Clicker
+                    key={ index }
+                    increment={ increment }
+                    keyName={ `count${index}` }
+                    color={ colors[index] }
+                />
+            ) }
+        </> }
+        <People />
+    </>
+}
